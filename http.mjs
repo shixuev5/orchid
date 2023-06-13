e05620fb6d229abc10292bbeb763c7f44451a191
Yan Hao <136393414@qq.com>
2023-06-13 09:54:57 +0800

    Update http.mjs

--- http.mjs
+++ http.mjs
@@ -1,7 +1,7 @@
 import { Axios } from 'axios';
 import axiosRetry from 'axios-retry';
 
-const http = new Axios({ proxy: process.env.noProxy ? false : { host: '127.0.0.1', port: '33210', protocol: 'http' } });
+const http = new Axios();
 axiosRetry(http, { retries: 3 });
 
 export {
